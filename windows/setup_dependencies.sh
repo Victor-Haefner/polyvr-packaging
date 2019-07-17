e808ae314ed26a91cca8dab11d6d5094dca3bd95
Your Name <you@example.com>
2019-07-17 17:51:52 +0200

    some fixes for windows port

--- windows/setup_dependencies.sh
+++ windows/setup_dependencies.sh
@@ -12,8 +12,18 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $DIR
 
+echo "Start installation of dependencies for PolyVR"
+echo " which VS Version? (2017/2019/..)"
+
+read VSversion
+VSgenerator="Visual Studio 15 2017 Win64"
+
+if [ $VSversion == "2019" ]; then
+VSgenerator="Visual Studio 16 2019"
+fi
+
 export VSCMD_START_DIR=$DIR
-/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/VC/Auxiliary/Build/vcvarsall.bat x86_amd64
+/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/$VSversion/Community/VC/Auxiliary/Build/vcvarsall.bat x86_amd64
 	
 # get boost -> TODO
 #  download boost binaries, see wiki on github!
@@ -32,13 +42,27 @@ export GLUT_INCLUDE_DIR="/c/freeglut"
 export GLUT_glut_LIBRARY_DEBUG="/c/freeglut/freeglutd.dll"
 export GLUT_glut_LIBRARY_RELEASE="/c/freeglut/freeglutd.dll"
 
-# get zlib
+# get dependencies
 cd $DIR
 if [ ! -e zlib/.git ]; then
 	git clone https://github.com/madler/zlib.git zlib
+fi
+
+if [ ! -e FreeGLUT/.git ]; then
+	git clone https://github.com/dcnieho/FreeGLUT.git FreeGLUT
+fi
+
+if [ ! -e OpenSG/.git ]; then
+	git clone https://github.com/Victor-Haefner/OpenSGDevMaster.git OpenSG
+fi
+
+# compile dependencies
+cd $DIR
+if [ ! -e zlib/build ]; then
+	echo "compile zlib"
 	mkdir zlib/build
 	cd zlib/build
-	cmake -G "Visual Studio 15 2017 Win64" ..
+	cmake -G "$VSgenerator" ..
 	cmake --build . --config Release
 	mkdir /c/zlib
 	cp zconf.h /c/zlib/
@@ -46,13 +70,12 @@ if [ ! -e zlib/.git ]; then
 	cp Release/* /c/zlib/
 fi
 
-# get freeglut
 cd $DIR
-if [ ! -e FreeGLUT/.git ]; then
-	git clone https://github.com/dcnieho/FreeGLUT.git FreeGLUT
+if [ ! -e FreeGLUT/build ]; then
+	echo "compile freeglut"
 	mkdir FreeGLUT/build
 	cd FreeGLUT/build
-	cmake -G "Visual Studio 15 2017 Win64" ../freeglut/freeglut
+	cmake -G "$VSgenerator" ../freeglut/freeglut
 	cmake --build . --config Release
 	mkdir /c/freeglut
 	cp bin/Release/* /c/freeglut/
@@ -61,29 +84,22 @@ if [ ! -e FreeGLUT/.git ]; then
 	cp -r ../freeglut/freeglut/include/* /c/freeglut/
 fi
 
-# get OpenSG source from git
-cd $DIR
-if [ ! -e OpenSG/.git ]; then
-  git clone https://github.com/Victor-Haefner/OpenSGDevMaster.git OpenSG
-fi
-
-# compile OpenSG
 cd $DIR
 if [ ! -e OpenSG/build ]; then
-  mkdir OpenSG/build
-  cd OpenSG/build
-  cmake -G "Visual Studio 15 2017 Win64" -DOSGBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DGLUT_INCLUDE_DIR="/c/freeglut" -DGLUT_glut_LIBRARY_RELEASE="/c/freeglut/freeglut.lib" -DZLIB_INCLUDE_DIR="/c/zlib" -DZLIB_LIBRARY_RELEASE="/c/zlib/zlib.lib" -DBoost_FILESYSTEM_LIBRARY_RELEASE="/c/boost/lib64-msvc-14.1/boost_filesystem-vc141-mt-1_65_1.lib" -DBoost_SYSTEM_LIBRARY_RELEASE="/c/boost/lib64-msvc-14.1/boost_system-vc141-mt-1_65_1.lib" ..
-  cmake --build . --config Release
-  d_inc=/c/opensg/include/OpenSG/
-  mkdir -p $d_inc
-  
-  cd $DIR
-  find OpenSG/Source -name "*.h" -exec cp {} $d_inc \;
-  find OpenSG/Source -name "*.inl" -exec cp {} $d_inc \;
-  find OpenSG/build/Source -name "*.h" -exec cp {} $d_inc \;
-  cp OpenSG/Source/WindowSystem/X/OSGNativeWindow.h $d_inc
-  cp -r OpenSG/build/bin/Release/* /c/opensg/
-  
+	echo "compile opensg"
+	mkdir OpenSG/build
+	cd OpenSG/build
+	cmake -G "$VSgenerator" -DOSGBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DGLUT_INCLUDE_DIR="/c/freeglut" -DGLUT_glut_LIBRARY_RELEASE="/c/freeglut/freeglut.lib" -DZLIB_INCLUDE_DIR="/c/zlib" -DZLIB_LIBRARY_RELEASE="/c/zlib/zlib.lib" -DBoost_FILESYSTEM_LIBRARY_RELEASE="/c/boost/lib64-msvc-14.1/boost_filesystem-vc141-mt-1_65_1.lib" -DBoost_SYSTEM_LIBRARY_RELEASE="/c/boost/lib64-msvc-14.1/boost_system-vc141-mt-1_65_1.lib" ..
+	cmake --build . --config Release
+	d_inc=/c/opensg/include/OpenSG/
+	mkdir -p $d_inc
+
+	cd $DIR
+	find OpenSG/Source -name "*.h" -exec cp {} $d_inc \;
+	find OpenSG/Source -name "*.inl" -exec cp {} $d_inc \;
+	find OpenSG/build/Source -name "*.h" -exec cp {} $d_inc \;
+	cp OpenSG/Source/WindowSystem/X/OSGNativeWindow.h $d_inc
+	cp -r OpenSG/build/bin/Release/* /c/opensg/
 fi
 
 
